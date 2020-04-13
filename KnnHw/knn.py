78784d0a5082876fb2ef4afc40a51d95b8312cf4
Seongjin Bak (SJB) <seongjinbak.sjb@gmail.com>
2020-04-13 12:19:16 +0900

    모든 함수 기능 구현 완료.
    k = 10일 경우 모든 데이터에서 100%의 정확도를 갖지만, 그 외에서는 테스트케이스 7에서 잘못된 데이터가 나온다. 이게 원래 그런지 확인 필요하다.

--- KnnHw/knn.py
+++ KnnHw/knn.py
@@ -7,39 +7,72 @@ class KNN:
         self.features = x
         self.target = y
         self.iris_names = iris_names
-        self.weighted_majority_vote = 0
+        self.weighted_majority_vote_value = 0
+        self.majority_vote_value = ''
         self.nearest_k = []
+        self.nearest_dist = []
 
     # 두 4차원 벡터의 거리를 구한다.
     def distance_metric(self, a, b):
         dist = 0
-        i = 0
-        while i < 4:
+        for i in range(len(self.features[0])):
             sub = a[i] - b[i]
             dist += (sub * sub)
-            i += 1
         return math.sqrt(dist)
 
     # 가장 가까운 k개의 데이터가 어떤 것인지 찾아내는 함수(k는 3, 5, 10..)
     def get_nearest_k(self, x_test):
-        iter_cnt = 0
         distance_dic = {}
-        while iter_cnt < len(self.features):
+        for iter_cnt in range(len(self.features)):
+            # distance_dic 딕셔너리에 key 값이 거리, value 값이 데이터의 번호인 원소를 넣는다.
             distance_dic[self.distance_metric(x_test, self.features[iter_cnt])] = iter_cnt
-            iter_cnt += 1
+
+        # 계산된 거리에 대해 오름차순으로 정렬한다.
         sdic = sorted(distance_dic.items())
-        iter_cnt = 0
-        while iter_cnt < self.k:
+
+        # 거리가 짧은 원소부터 k개 수집한다.
+        for iter_cnt in range(self.k):
             self.nearest_k.append(sdic[iter_cnt][1])
-            iter_cnt += 1
+            self.nearest_dist.append((sdic[iter_cnt][0]))
 
-    # 아웃풋은 MV WMV가 있기 때문에, K가 3개이므로 총 6개를 레포트에 붙혀넣기.
-    def obtain_majority_vote(self):
-        return 'a'
+    # 아웃풋은 MV WMV 가 있기 때문에, K가 3개이므로 총 6개를 레포트에 붙혀넣기.
+    def majority_vote(self):
+        majority_cnt = []
 
-    def obtain_weighted_majority_vote(self):
-        return 'k'
+        # class 개수의 길이를 가지는 리스트를 만든다.
+        for i in range(len(self.iris_names)):
+            majority_cnt.append(0)
+
+        # nearest k의 이름에 해당하는 class 의 카운트를 증가시킨다.
+        for i in range(self.k):
+            majority_cnt[self.target[self.nearest_k[i]]] += 1
+
+        # 가장 많은 꽃 class 가 속한 이름을 찾아 저장한다.
+        self.majority_vote_value = self.iris_names[majority_cnt.index(max(majority_cnt))]
+        return self.majority_vote_value
+
+    def weighted_majority_vote(self):
+        majority_cnt = []
+
+        # class 개수의 길이를 가지는 리스트를 만든다.
+        for i in range(len(self.iris_names)):
+            majority_cnt.append(1)
+
+        # nearest k의 이름에 해당하는 class 의 카운트를 증가시킨다.
+        for i in range(self.k):
+            # 가중치는 거리의 역수로 취하고, 가중치가 너무 커지는 것을 방지하기 위해 거리의 역수의 로그값을 가중치로 한다.
+            weight = math.log10(1 / self.nearest_dist[i])
+            majority_cnt[self.target[self.nearest_k[i]]] += (majority_cnt[self.target[self.nearest_k[i]]] * weight)
+
+        # 가장 높은 수치를 가진 꽃 class 가 속한 이름을 찾아 저장한다.
+        self.weighted_majority_vote_value = self.iris_names[majority_cnt.index(max(majority_cnt))]
+        return self.weighted_majority_vote_value
 
     def reset(self):
-        return ';'
+        self.weighted_majority_vote_value = 0
+        self.majority_vote_value = ''
+        self.nearest_k = []
+        self.nearest_dist = []
 
+    def show_dim(self):
+        return len(self.features[0])
--- KnnHw/main.py
+++ KnnHw/main.py
@@ -1,3 +1,4 @@
+import inline as inline
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,13 +6,10 @@ from sklearn.datasets import load_iris
 from knn import KNN
 
 iris = load_iris()
-# print(iris)
 
 X = iris.data  # iris data input
 y = iris.target  # iris target (label)
 y_name = iris.target_names
-
-# print(y_name)
 
 # 트레이닝 데이터와 테스트 데이터를 나누는 부분.
 l = 15
@@ -32,15 +30,26 @@ y_train = y[for_train]
 X_test = X[for_test]
 y_test = y[for_test]
 
-# print(X_train)
-# print(y_train)
-knn_iris = KNN(10, X_train, y_train, y_name)
-# knn_iris.show_dim()
+# 산포도 생성을 위한 리스트 생성, 할당.
+sepal_length = np.array(X_train[:, 0])
+sepal_width = np.array(X_train[:, 1])
+petal_length = np.array(X_train[:, 2])
+petal_width = np.array(X_train[:, 3])
+# plt 를 사용하여 산포도 그래프 생성
+plt.title('B511086 Seongjin Bak Iris data set')
 
-# print(X_train[0])
-# print(knn_iris.distance_metric(X_train[0], X_train[1]))
-knn_iris.get_nearest_k(X_test[0])
-# for i in range(y_test.shape[0]):
-    # knn_iris.get_nearest_k(X_test[i])
-    # print("Test Data: ", i, " Computed class: ", knn_iris.weighted_majority_vote(), ", \tTrue class: ", y_name[y_test[i]])
-    # knn_iris.reset()
+testnum = 7
+plt.scatter(X_test[testnum][0], X_test[testnum][1], c='red', s=50 * X_test[testnum][3])
+plt.scatter(sepal_length, sepal_width, alpha=0.6, s=50 * petal_width, c=petal_length)
+plt.xlabel('Sepal length', fontsize=10)
+plt.ylabel('Sepal width', fontsize=10)
+plt.show()
+
+knn_iris = KNN(1, X_train, y_train, y_name)
+knn_iris.show_dim()
+
+for i in range(y_test.shape[0]):
+    knn_iris.get_nearest_k(X_test[i])
+    print("Test Data: ", i, " Computed class: ", knn_iris.weighted_majority_vote(), ", \tTrue class: ",
+          y_name[y_test[i]])
+    knn_iris.reset()
