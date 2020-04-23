cb9d655a8802d308d087b2ae6576d1823aed1350
Seongjin Bak (SJB) <seongjinbak.sjb@gmail.com>
2020-04-23 21:55:22 +0900

    MNIST KNN 용 MAIN 파일 생성

---
+++ MNIST_KnnHw/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+import sys, os
+sys.path.append(os.pardir)
+# 부모 디렉터리에서 import 할수있도록 설정
+
+from dataset.mnist import load_mnist
+# mnist data load 할 수 있는 함수 import
+
+from PIL import Image
+
+
+
