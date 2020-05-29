4aa299a428f6a88839206678c7dcc0125d35c74e
Seongjin Bak (SJB) <seongjinbak.sjb@gmail.com>
2020-05-29 13:51:45 +0900

    과제 4 완료

--- HW4_TwoLayerNeuralNetwork/iris_main.py
+++ HW4_TwoLayerNeuralNetwork/iris_main.py
@@ -1,4 +1,4 @@
-# B511086 백성진
+# B511086 백성진 HW4
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,18 +15,15 @@ X = iris.data
 y = iris.target
 y_name = iris.target_names
 
-# 150개 중 train 데이터로 중복없는 120개의 데이터를, 테스트 데이터로 나머지 30개 데이터를 얻는다.
+# 150개 중 train 데이터로 중복없는 120개의 데이터를, 테스트 데이터로 나머지 30개 데이터를 얻습니다.
 for_train = np.array(random.sample(range(y.shape[0]), 120))
 for_test = np.array([i for i in range(y.shape[0]) if np.int(i) not in for_train])
 
 # 훈련용 데이터, 테스트용 데이터 x 설정.
 x_train = X[for_train]
-#y_train = y[for_train]
-
 x_test = X[for_test]
-#y_test = y[for_test]
 
-# 타겟들을 One-hot encoding 으로 변환
+# 타겟들을 One-hot encoding 으로 변환합니다.
 num = np.unique(y[for_train], axis=0)
 num = num.shape[0]
 y_train = np.eye(num)[y[for_train]].astype(np.int)
@@ -35,52 +32,69 @@ num = np.unique(y[for_test], axis=0)
 num = num.shape[0]
 y_test = np.eye(num)[y[for_test]].astype(np.int)
 
-
 # 2000 20 0.05 5
-iter_num = 1000
+
+# 학습 반복 횟수
+iter_num = 900
+# 입력 훈련용 데이터의 크기
 train_size = x_train.shape[0]
+# 배치 사이즈. 120개 중 batch_size 개를 사용합니다.
 batch_size = 20
-learning_rate = 0.05
+# 학습률
+learning_rate = 0.01
+# 히든 레이어의 개수
 hidden_layer_num = 5
 
+# 클래스 오브젝트 생성. 4는 input, 3은 output class 을 의미합니다.
 two_layer_network = NeuralNetworkClass(4, hidden_layer_num, 3, learning_rate)
 
-iter_per_epoch = max(train_size / batch_size, 1)
 
-train_loss_list = []
+# 그래프를 위한 매개 리스트
+loss_list = []
 train_acc_list = []
 test_acc_list = []
+
+# 최종 정확도를 위한 변수 입니다.
+final_test_acc = None
+final_training_acc = None
 
 for i in range(iter_num):
     # 미니배치 획득 ( 120개 중 batch size 개 만큼)
     batch_sample = np.random.choice(train_size, batch_size)
 
+    # 얻어낸 샘플 데이터의 인덱스를 batch 데이터로 할당합니다.
     x_batch = x_train[batch_sample]
     y_batch = y_train[batch_sample]
 
-    # 배치 데이터 저장
+    # 배치 데이터를 클래스 오브젝트에 저장합니다.
     two_layer_network.set_batch_data(x_batch, y_batch)
 
-    # 학습
+    # 학습 실시합니다.
     two_layer_network.learn()
 
-    # 학습 진행상황 기록
+    # 손실 값 기록
     loss = two_layer_network.loss()
 
-    # 1에폭당 정확도 계산
-    if i % iter_per_epoch == 0:
-        train_acc = two_layer_network.accuracy(x_train, y_train)
-        test_acc = two_layer_network.accuracy(x_test, y_test)
-        train_loss_list.append(loss)
-        train_acc_list.append(train_acc)
-        test_acc_list.append(test_acc)
-        print(i, "train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+    # 훈련용 데이터의 정확도 기록
+    train_acc = two_layer_network.accuracy(x_train, y_train)
 
-# 그래프 그리기
+    # 테스트 데이터의 정확도 기록
+    test_acc = two_layer_network.accuracy(x_test, y_test)
+    
+    loss_list.append(loss)
+    train_acc_list.append(train_acc)
+    print(i, 'cost, accuracy', loss, train_acc)
 
+    if i + 1 == iter_num:
+        final_test_acc = test_acc
+        final_training_acc = train_acc
 
-plt.plot(train_acc_list, label='train acc')
-plt.plot(train_loss_list, label='train loss')
+# 최종 정확도 출력합니다.
+print('Training Accuracy =', final_training_acc)
+print('Test Accuracy =', final_test_acc)
 
+# 그래프 출력
+plt.plot(loss_list, label='loss')
+plt.plot(train_acc_list, label='training accuracy')
 plt.legend(loc='upper right')
 plt.show()
--- HW4_TwoLayerNeuralNetwork/neural_network_class.py
+++ HW4_TwoLayerNeuralNetwork/neural_network_class.py
@@ -1,51 +1,76 @@
+# B511086 백성진 HW4
+
+
 import numpy as np
 
 
 class NeuralNetworkClass:
     # 생성자 매개변수 : 입력층의 노드 수, 은닉층의 노드 수, 출력층의 노드 수
     def __init__(self, input_size, hidden_size, output_size, learning_rate):
-        # 정규분포 된 랜덤 값을 w 에는 input size x hidden size 행렬로 지정
+        # 가중치를 정규분포 된 랜덤 값으로 지정
         # W1: x --> hidden layer
-        # W2: x --> output layer
+        # W2: hidden layer --> output layer
+        # b1: W1 과정에 첨가되는 bias
+        # b2: W2 과정에 첨가되는 bias
         self.params = {'W1': np.random.randn(input_size, hidden_size), 'b1': np.random.randn(hidden_size),
                        'W2': np.random.randn(hidden_size, output_size), 'b2': np.random.randn(output_size)}
 
-        # 사이즈 저장
+        # 데이터 사이즈
         self.input_size = input_size
         self.output_size = output_size
 
-        # 배치한 데이터
+        # 학습을 위해 미니 배치한 데이터
         self.batch_x = 0
-        self.batch_y = 0
+        self.batch_t = 0
 
         # 학습률
         self.lr = learning_rate
 
+    """
+    예측함수 입니다. 매개변수로 받는 x로 예측을 원하는 데이터가 들어옵니다.
+    본 과제에서는 층이 2개 입니다.
+    """
     def predict(self, x):
-        W1 = self.params['W1']
-        W2 = self.params['W2']
-        b1 = self.params['b1']
-        b2 = self.params['b2']
 
-        a1 = np.dot(x, W1) + b1
+        # 입력 -> 1층 단계 입니다.
+        # 모든 가중치를 입력 데이터와 행렬곱 연산한 후 편향 데이터를 더합니다.
+        a1 = np.dot(x, self.params['W1']) + self.params['b1']
+        # 결과 값을 활성 함수인 시그모이드 함수에 넣습니다.
         z1 = sigmoid(a1)
-
-        a2 = np.dot(z1, W2) + b2
+        # 1층 -> 2층 단계 입니다.
+        # 활성 값을 가중치와 행렬곱 연산한 후 편향 데이터를 더합니다.
+        a2 = np.dot(z1, self.params['W2']) + self.params['b2']
+        # 결과 값을 소프트맥스 함수로 계산하여 각 입력 데이터의 분류 확률을 얻습니다.
         y = softmax(a2)
         return y
 
-    # x 는 입력 데이터, t 는 테스트 데이터 ???
+    """
+    손실함수 입니다.
+    본 과제에서는 미니배치를 사용하기 때문에, 매번 새로운 입력 데이터가 들어옵니다.
+    이 입력데이터와 정답 데이터를 매개변수로 넘기지 않고, 클래스의 멤버변수로 지정한 데이터를 가져옵니다.
+    """
     def loss(self):
+        # x는 입력데이터, t는 정답데이터 입니다.
         x, t = self.get_batch_data()
-        y = self.predict(x)  # (batch size, 3)
+        # y 에 입력데이터를 예측한 결과를 저장합니다. 결과는 batch_size, 3의 형상을 가집니다.
+        y = self.predict(x)
+        # 예측 결과를 교차 엔트로피 함수로 실제 값과 예측값의 오차를 계산합니다.
         res = cross_entropy_error(y, t)
         return res
 
+    """
+    정확도 계산 함수 입니다.
+    훈련용 데이터, 테스트 데이터의 정확도를 계산하는데 쓰이므로, 범용성을 위해 미니배치 데이터로 예측을 하는것이 아닌
+    매개변수로 들어온 값으로 예측을 하도록 하였습니다.
+    """
     def accuracy(self, x, t):
+        # 예측
         y = self.predict(x)
+        # 예측 값의 각 행마다 가장 큰 확률을 가지는 원소의 인덱스를 저장합니다.
         y = np.argmax(y, axis=1)
-
+        # 정답 데이터는 원-핫 인코딩 되어있으므로, 마찬가지로 0이 아닌, 1을 가지는 인덱스를 t에 저장합니다.
         t = np.argmax(t, axis=1)
+        # 정확도 계산
         cnt = 0
         correct = 0
         for i in range(len(y)):
@@ -55,61 +80,96 @@ class NeuralNetworkClass:
 
         return correct / cnt
 
+    # 매개변수로 클래스에 배치 데이터 저장하는 함수
     def set_batch_data(self, x, t):
         self.batch_x = x
-        self.batch_y = t
+        self.batch_t = t
 
+    # 클래스의 배치 데이터 반환하는 함수
     def get_batch_data(self):
-        return self.batch_x, self.batch_y
+        return self.batch_x, self.batch_t
 
-    # 기울기 계산용 손실함수
-    def loss_func(self, n):
-        return self.loss()
+    """
+    기울기 계산 함수.
+    매개변수 x로 가중치 파라미터를 전달받습니다.
+    가중치 파라미터의 각 value 로 수치미분을 진행합니다.
+    딕셔너리의 value 는 mutable 하므로, 함수 내부에서 변형시키면 원본 딕셔너리에 영향이 갑니다.
+    따라서, value 를 수정한 후, loss 진행을 하며 그 후 원래의 값으로 되돌립니다.
+    """
+    def calculate_gradient(self, x):
+        # 아주 작은 h 값.
+        h = 1e-4
 
-    def calculate_gradient(self, f, x):
-        h = 1e-4  # 0.0001
-        gradients = np.zeros_like(x)  # x와 형상이 같은 배열을 생성
+        # x와 형상이 같은 배열을 생성합니다.
+        gradients = np.zeros_like(x)
 
+        # 넘어온 가중치의 각 value 당 루프를 돌며, 수치미분을 합니다.
         for idx in range(len(x)):
+
+            # 넘어온 가중치의 value 저장.
             tmp_val = x[idx]
 
             # f(x+h) 계산
             x[idx] = float(tmp_val) + h
-            fxh1 = f(x)
+
+            # x + h 가중치로 손실 측정
+            fxh1 = self.loss()
 
             # f(x-h) 계산
             x[idx] = tmp_val - h
-            fxh2 = f(x)
 
+            # x - h 가중치로 손실 측정
+            fxh2 = self.loss()
+
+            # 기울기 계산
             gradients[idx] = (fxh1 - fxh2) / (2 * h)
-            x[idx] = tmp_val  # 값 복원
+
+            # 값을 원래대로 되돌립니다.
+            x[idx] = tmp_val
 
         return gradients
 
-    # x : 입력 데이터, t : 정답 레이블
+    """
+    기울기 계산의 caller 함수.
+    본 과제의 모든 가중치에 대해 기울기를 최신화 합니다.
+    bias 값은 1차원 이지만, W 값은 2차원 행렬 이므로 각 행의 모든 열에 대해 기울기 계산을 해줍니다.
+    """
     def numerical_gradient(self):
+        # 저장할 가중치 딕셔너리
         gradients = {}
 
+        # W1 에 대해 가중치 계산 진행. input x hidden layer
         grad = np.zeros_like(self.params['W1'])
+        # input 행 당 모든 열에 대해 진행
         for i in range(len(self.params['W1'])):
-            grad[i] = self.calculate_gradient(self.loss_func, self.params['W1'][i])
+            grad[i] = self.calculate_gradient(self.params['W1'][i])
 
         gradients['W1'] = grad
 
+        # W2 에 대해 가중치 계산 진행. hidden layer x output
         grad = np.zeros_like(self.params['W2'])
+        # hidden layer 행 당 모든 열에 대해 진행
         for i in range(len(self.params['W2'])):
-            grad[i] = self.calculate_gradient(self.loss_func, self.params['W2'][i])
+            grad[i] = self.calculate_gradient(self.params['W2'][i])
 
         gradients['W2'] = grad
 
-        gradients['b1'] = self.calculate_gradient(self.loss_func, self.params['b1'])
-        gradients['b2'] = self.calculate_gradient(self.loss_func, self.params['b2'])
+        # bias 에 대해 진행. b1 은 hidden layer, b2 는 output 의 개수.
+        gradients['b1'] = self.calculate_gradient(self.params['b1'])
+        gradients['b2'] = self.calculate_gradient(self.params['b2'])
 
         return gradients
 
+    """
+    학습 함수 입니다.
+    배치 데이터를 받아와, 그 데이터에 대해 가중치의 기울기 값을 구한 후, 가중치를 최신화 합니다.
+    """
     def learn(self):
+        # 배치 데이터 얻음.
         x, t = self.get_batch_data()
+        # 각 가중치의 예측 후의 손실 값 얻음.
         gradients = self.numerical_gradient()
+        # 최신화 된 값을 학습률에 곱하여, 원래의 가중치에서 뺌.
         self.params['W1'] -= self.lr * gradients['W1']
         self.params['b1'] -= self.lr * gradients['b1']
         self.params['W2'] -= self.lr * gradients['W2']
@@ -125,24 +185,31 @@ def sigmoid(z):
 
 # 소프트맥스 함수
 def softmax(x):
-    # exp_x = np.exp(x)   # x가 커질수록 값이 너무 커지므로 컴퓨터가 감당 불가 하므로 아래와같이 오버플로우 대비한다
+
+    # 뺄셈 계산을 위해 (x.shape[0], )를 (x.shape[0], 3)으로 변환합니다.
+    # 각 행당 가장 큰 값을 얻음
     t = np.max(x, axis=1)
+    # x와의 형상 통일을 위한 append.
     t = t.reshape(x.shape[0], 1)
     t1 = np.append(t, t, axis=1)
     t = np.append(t1, t, axis=1)
 
+    # 지수 함수의 특성으로 인한 아주 큰 값을 방지하기 위해, 가장 큰 값을, 각 행의 전체 원소에 뺄셈 합니다.
     exp_x = np.exp(x - t)
 
+    # 계산을 위해 (x.shape[0], )를 (x.shape[0], 3)으로 변환합니다.
+    # x와의 형상 통일을 위한 append.
     sum_exp_x1 = np.sum(exp_x, axis=1)
     sum_exp_x1 = sum_exp_x1.reshape(x.shape[0], 1)
     sum_exp_x2 = np.append(sum_exp_x1, sum_exp_x1, axis=1)
     sum_exp_x = np.append(sum_exp_x2, sum_exp_x1, axis=1)
 
     y = exp_x / sum_exp_x
-    print(y)
     return y
 
 
+# 교차 엔트로피 오류 함수.
+# 매개변수로 들어온 y 는 예측값, t는 원-핫 인코딩된 정답 값 입니다.
 def cross_entropy_error(y, t):
     epsilon = 1e-7
 
@@ -152,5 +219,7 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape[0]
 
+    # 들어온 데이터의 오차 값을 계산합니다.
+    # 값이 클 수록, 오답일 가능성이 높다는 것을 나타냅니다.
     cee = np.sum(-t * np.log(y + epsilon)) / batch_size
     return cee
